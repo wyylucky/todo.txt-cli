faf86c88cb28112638d484d612d7afa9c144d1fe
Ingo Karkat <swdev@ingo-karkat.de>
2011-07-10 14:31:20 +0200

    nobacktick code check: Skip entire comment lines, too.

--- tests/t0100-code-nobacktick.sh
+++ tests/t0100-code-nobacktick.sh
@@ -5,11 +5,13 @@ test_description='no old-style backtick command substitution
 This test checks the todo.sh script itself for occurrences
 of old-style backtick command substitution, which should be
 replaced with $(...).
+On failure, it will print each offending line number and line.
 '
 . ./test-lib.sh
 
 test_todo_session 'no old-style backtick command substitution' <<EOF
->>> sed -n -e 's/[ \t]#.*//' -e '/\d96/{=;p}' "$(which todo.sh)"
+>>> sed -n -e 's/\(^\|[ \t]\)#.*//' -e '/\d96/{=;p}' "$(which todo.sh)"
 EOF
+# Note: Must encode backtick as \d96 to avoid error in the test framework.
 
 test_done
