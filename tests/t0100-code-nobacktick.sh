728bc564a1bbc263a35de83feabed3f92862e589
Ingo Karkat <swdev@ingo-karkat.de>
2011-07-18 13:41:38 +0200

    nobacktick test: Use literal backtick in test.
    With proper quoting, the non-POSIX \d96 escape isn't necessary, and the check should work also on Mac OS.

--- tests/t0100-code-nobacktick.sh
+++ tests/t0100-code-nobacktick.sh
@@ -10,8 +10,7 @@ On failure, it will print each offending line number and line.
 . ./test-lib.sh
 
 test_todo_session 'no old-style backtick command substitution' <<EOF
->>> sed -n -e 's/\(^\|[ \t]\)#.*//' -e '/\d96/{' -e '=;p' -e '}' "$(which todo.sh)"
+>>> sed -n -e 's/\(^\|[ \t]\)#.*//' -e '/\`/{' -e '=;p' -e '}' "$(which todo.sh)"
 EOF
-# Note: Must encode backtick as \d96 to avoid error in the test framework.
 
 test_done
